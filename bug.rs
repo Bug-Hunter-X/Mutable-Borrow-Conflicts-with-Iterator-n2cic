64508ca8e09ad00cc6e56ff0544e3815994c0c5f
BugHunter-X <askzenai@gmail.com>
2024-12-31 20:50:04 +0100

    Add bug.rs file

---
+++ bug.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let mut vec = Vec::new();
+    vec.push(1);
+    vec.push(2);
+    vec.push(3);
+
+    let mut iter = vec.iter();
+
+    println!("First element: {:?}", iter.next());
+    println!("Second element: {:?}", iter.next());
+
+    //Error: Cannot borrow `vec` as mutable because it is also borrowed as immutable
+    vec.push(4); 
+}
